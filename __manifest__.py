6e6be18f0874d3815275435d74f9ca466d36cbfd
Jhonsson Córdova <ionszon1806@gmail.com>
2022-11-28 14:42:57 -0500

    0 Test 2 commit

--- __manifest__.py
+++ __manifest__.py
@@ -8,6 +8,19 @@
         'security/ir.model.access.csv',
 
         'views/coordenadas_views.xml',
+        'views/transformador_views.xml',
+        'views/punto_carga_views.xml',
+        'views/tipo_vinculacion_views.xml',
+        'views/tipo_consumidor_views.xml',
+        'views/marca_medidor_views.xml',
+        'views/provincia_views.xml',
+        'views/canton_views.xml',
+        'views/red_views.xml',
+        'views/balance_energetico_views.xml',
+        'views/consumidor_views.xml',
+        'views/vinculacion_views.xml',
+
+
         'views/balances_menu_views.xml',
     ]
 }
